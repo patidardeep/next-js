30fa61291d5d3501d827e4cdcc477c7062da38f0
patidardeep <patidardeepak327@gmail.com>
2023-06-29 11:04:17 +0530

    Basic Routing and New pages

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+
+ const login = ()=>{
+    return(
+        <div>
+            <h1>
+                Login Page
+            </h1>
+        </div>
+    )
+ }
+
+ export default login;
--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,8 @@
-'use client'
-
-import { useState } from "react"
-
-export default function Home(){
-// const apple=(item)=>{
-// alert("fruit")
-// alert(item)
-// state--
-
-const [name,setName] = useState("Deepak")
-const Changename= ()=>{
-  setName("Shubham")
-}
-// inner componet
-
-const InnerComp = ()=>{
+ export default function Home(){
   return(
     <div>
-      <h1>Inner comp</h1>
-      <h2>heloow world thi is example of Componet inside componet</h2>
-    </div>
-  )
-}
-
-
-  return(
-    <div>
-      <h1> Evevent  ,Function and State</h1>
-      {/* <button onClick={()=>alert("hello next js")}>Click</button> */}
-      {/* <button onClick={apple}>Click</button> */}
-      {/* <button onClick={()=>apple("fruits")}>Click</button> */}
-
-<h1>hello {name}</h1>
-<button onClick={Changename}>Change Name</button>
-<InnerComp/>
-
+      <h1>Basic Routing | Make New Page</h1>
       
     </div>
   )
-}
-
-
+ }
