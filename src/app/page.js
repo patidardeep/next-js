1370635eb3bfbebc10007eee1ce317ec049ffe61
patidardeep <patidardeepak327@gmail.com>
2023-07-01 09:58:29 +0530

    condition with style

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
+'use client'
+import { useState } from "react"
+import style from './style.module.css'
 
-import custom from './custom.module.css'
-import other from './other.module.css'
-import outside from '@/outside.module.css'
 export default function Home(){
-  return(
-    <div>
-<h1 className={custom.main}>CSS module with Nextjs </h1>  
-<h1 className={other.main}>Heading 2</h1>  
-<h3 className={outside.main}>ouside module css</h3>
-
-    </div>
-  )
+  const [color,setColor] = useState("red")
+  const {red} = style
+return(
+  <div>
+    <h1 className={color =="red"?style.red:style.green}>Condition  with Style</h1>
+    <h2 style={{backgroundColor:color=="red"?'red':'green'}}>Heading2 </h2>
+    <h3 id={style.orange}>Heading 3</h3>
+    <h4 className={red}>Deepak</h4>
+    <h4>Deepak</h4>
+    <h4>Deepak</h4>
+    <button onClick={()=>setColor("green")}>Update Color</button>
+  </div>
+)
 }
