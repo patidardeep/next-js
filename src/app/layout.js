ba8e15fae23fe5cf807dd57f5fab4e898303bf56
patidardeep <patidardeepak327@gmail.com>
2023-07-02 10:31:56 +0530

    Font Implimantation

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@500&family=Poppins:ital,wght@0,300;0,400;0,500;1,100;1,600&family=Roboto:ital,wght@0,100;0,300;1,100&display=swap" rel="stylesheet"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,11 @@
-import Image from "next/image"
-import Profile from "../../public/img.jpg"
 
 export default function Home(){
-  console.log(Profile)
+  
 return(
   <div>
-   <h1>Image Optimization in Next</h1>
-{/* <Image
-src={Profile}
-/>
+   <h1>Font Implimantion In Next Js</h1>
+<h2 style={{fontFamily:"Roboto",fontWeight:100}}>Font change By Linking Tag</h2>
 
-<img
-src={Profile.src}
-/> */}
-<Image
-src="https://unsplash.com/photos/a-close-up-of-a-building-with-a-clock-on-it-7n289pEjSZ0"
-width={200}
-height={200}
-/>
 
   </div>
 )
