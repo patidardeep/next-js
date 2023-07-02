aa9a08e4b9cad94bb6ecce18300ad39856efe10a
patidardeep <patidardeepak327@gmail.com>
2023-07-02 10:39:43 +0530

    Dynamic Meta data

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ export default function Home(){
   
 return(
   <div>
-   <h1>Font Implimantion In Next Js</h1>
-<h2 style={{fontFamily:"Roboto",fontWeight:100}}>Font change By Linking Tag</h2>
+  <h1>Dynamic Metadata</h1>
 
 
   </div>
---
+++ src/app/user/page.js
@@ -0,0 +1,15 @@
+export default function User(){
+return(
+    <div>
+        <h1>User Page</h1>
+    </div>
+)
+}
+
+export function generateMetadata(){
+    return{
+        title:"user page title",
+        description:"this is description OF user Page"
+
+    }
+}
